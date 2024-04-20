b9d0decb2a23f68c612396d6834c797e2e8e1af1
MatteoLorenzini96 <matteolorenzini96.ml@gmail.com>
2024-04-20 17:15:56 +0200

    Fixed Start Scene

--- Assets/Scripts/StartScene/Start.cs
+++ Assets/Scripts/StartScene/Start.cs
@@ -1,9 +1,12 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Start : MonoBehaviour
 {
     public string nextSceneName;
+    public TextMeshProUGUI infoText; // Riferimento all'elemento TextMesh Pro
 
     public Transform references;
     public void PassaScenaSuccessiva()
@@ -15,7 +18,17 @@ public class Start : MonoBehaviour
         }
         else
         {
-            Debug.LogWarning("Impossibile passare alla scena successiva: l'oggetto References ha ancora figli al suo interno.");
+            StartCoroutine(DisplayInfoForSeconds("The Title is not in place", 2f));
         }
+    }
+
+    private IEnumerator DisplayInfoForSeconds(string info, float duration)
+    {
+        infoText.text = info; // Imposta il testo dell'elemento TextMesh Pro
+        infoText.gameObject.SetActive(true); // Attiva l'elemento TextMesh Pro
+
+        yield return new WaitForSeconds(duration);
+
+        infoText.gameObject.SetActive(false); // Disattiva l'elemento TextMesh Pro dopo 'duration' secondi
     }
 }
