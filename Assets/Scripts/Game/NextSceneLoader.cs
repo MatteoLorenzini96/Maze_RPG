dd609f079791026c77a72c92d871a340a5940142
MatteoLorenzini96 <matteolorenzini96.ml@gmail.com>
2024-04-22 19:37:21 +0200

    On Tresure Player Stop Move

--- Assets/Scripts/Game/NextSceneLoader.cs
+++ Assets/Scripts/Game/NextSceneLoader.cs
@@ -6,6 +6,7 @@ public class NextSceneLoader : MonoBehaviour
     public GameObject oggettoDaAttivare; // Oggetto da attivare al raggiungimento del player
     public Collider2D playerCollider;
     public ContaMovimenti contaMovimenti; // Riferimento al componente ContaMovimenti per interrompere il calcolo dei movimenti
+    public PlayerMovement playerMovement; // Riferimento al componente di movimento del player
 
     public Button PassButton;
 
@@ -18,6 +19,10 @@ public class NextSceneLoader : MonoBehaviour
         if (contaMovimenti == null)
         {
             Debug.LogError("ContaMovimenti not assigned in NextSceneLoader.");
+        }
+        if (playerMovement == null)
+        {
+            Debug.LogError("PlayerMovement not assigned in NextSceneLoader.");
         }
     }
 
@@ -36,6 +41,12 @@ public class NextSceneLoader : MonoBehaviour
             if (contaMovimenti != null)
             {
                 contaMovimenti.enabled = false;
+            }
+
+            // Disabilita il movimento del player
+            if (playerMovement != null)
+            {
+                playerMovement.enabled = false;
             }
 
             // Disattiva questo script
